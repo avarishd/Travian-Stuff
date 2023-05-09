2e7c2b3b27e74ce356457996255d48af6ddc87cb
avarishd <46330494+avarishd@users.noreply.github.com>
2023-05-09 16:17:21 +0300

    Create Travian NoScroll.user.js

---
+++ Travian NoScroll.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Travian NoScroll
+// @namespace    https://github.com/avarishd/Travian-Stuff
+// @version      1
+// @description  Simple script that removes the top row buttons.
+// @author       Avarishd
+
+// @include     *://*.travian.*/report*
+// @include     *://*.travian.*/messages*
+// ==/UserScript==
+
+if (window.location.pathname == '/report')
+{
+    document.getElementsByClassName('buttonWrapper')[0].style.display = 'none';
+    document.getElementsByClassName('footer')[0].style.display = 'none';
+}
+
+else if (window.location.pathname == '/messages/inbox')
+{
+    document.getElementsByClassName('footer')[0].style.display = 'none';
+    document.getElementById("mark").style.display = "none";
+    document.getElementById("delete").style.display = "none";
+}
+
+else if (window.location.pathname == '/messages/sent')
+{
+    document.getElementsByClassName('footer')[0].style.display = 'none';
+    document.getElementById("delete").style.display = "none";
+}
